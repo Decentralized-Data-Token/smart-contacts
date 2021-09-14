23042487e57bbc960884abc8232e774ec0f202d1
deda-official <87776847+deda-official@users.noreply.github.com>
2021-09-15 01:54:59 +0700

    fix: pragma condition

--- contracts/DedaToken.sol
+++ contracts/DedaToken.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.5.16;
+pragma solidity >=0.5.16;
 
 import "./libs/BEP20.sol";
 
-contract DedaToken is
-    BEP20("Decentralized Data Token", "DEDA", 8, 10000000000000000)
-{
+contract DedaToken is BEP20("Deda Counter", "DEDAC", 8, 10000000000000000) {
     constructor() public {}
 }
